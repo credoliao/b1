0c550dc28f1a8aac5b88e3ce65588b869f1cba6d
credoliao <credoliao@asec.com.tw>
2015-12-30 13:52:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/b1/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.b1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.File;
+import java.util.Properties;
+
+import com.google.gson.JsonObject;
+import com.ibm.iotf.client.device.DeviceClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Provide the device specific data using Properties class
+        Properties options = new Properties();
+        options.setProperty("org", "xs930m");
+        options.setProperty("type", "androidApp");
+        options.setProperty("id", "android01");
+        options.setProperty("auth-method", "token");
+        options.setProperty("auth-token", "asec1234");
+
+        DeviceClient myClient = null;
+        try {
+            //Instantiate the class by passing the properties file
+            myClient = new DeviceClient(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //Connect to the IBM IoT Foundation
+        myClient.connect();
+
+        //Generate a JSON object of the event to be published
+        JsonObject event = new JsonObject();
+        event.addProperty("name", "foo");
+        event.addProperty("cpu",  90);
+        event.addProperty("mem",  70);
+
+        //Quickstart flow allows only QoS = 0
+        myClient.publishEvent("status", event, 0);
+        System.out.println("SUCCESSFULLY POSTED......");
+    }
+}
